cf34d039854bbec44b5001815d7cef8b1dfbea2e
Byron <byron.walkermills@joipolloi.com>
2023-04-19 12:39:42 +0100

    Add logos to start page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,26 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between min-h-screen bg-[#f0b4d7] max-w-sm w-full mx-auto px-6 py-12 text-center">
-      <div>
-        <img src="/img-start-top.png" alt="" />
+    <div className="bg-[#f0b4d7]">
+      <div className="flex flex-col justify-between min-h-screen max-w-sm w-full mx-auto px-6 py-12 text-center">
+        <div className="flex items-center justify-center space-x-6">
+          <span><img src="/img-leeds.png" alt="" className="max-w-full" /></span>
+          <span><img src="/img-wow.png" alt="" className="max-w-full" /></span>
+          <span><img src="/img-space.png" alt="" className="max-w-full" /></span>
+        </div>
+
+        <div className="space-y-6">
+          <h1 className="text-[#a50c9d] font-serif text-6xl uppercase">
+            <span className="block font-bold">One Way</span>
+            <span className="block italic">or</span>
+            <span className="block font-bold">Another</span>
+          </h1>
+
+          <p className="text-lg font-bold text-[#a50c9d] uppercase tracking-widest">An Interactive Experience</p>
+        </div>
+
+        <Link href="/intro" className="text-lg text-center uppercase tracking-widest w-full p-2.5 bg-[#a50c9d] border-5 border-[#7b0575] text-white shadow-sm transition">Start</Link>
       </div>
-
-      <div>
-        <h1 className="text-[#a50c9d] font-serif text-6xl mb-6 uppercase">
-          <span className="block font-bold">One Way</span>
-          <span className="block italic">or</span>
-          <span className="block font-bold">Another</span>
-        </h1>
-
-        <p className="text-lg font-bold text-[#a50c9d] uppercase tracking-widest mb-12">An Interactive Experience</p>
-      </div>
-
-      <Link href="/intro" className="text-lg text-center uppercase tracking-widest w-full p-2.5 bg-[#a50c9d] border-5 border-[#7b0575] text-white shadow-sm transition">Start</Link>
     </div>
   )
 }

260e3ddc2ac7c188d5fb2aa314a9330206205e00
Byron <byron.walkermills@joipolloi.com>
2023-05-04 11:44:16 +0100

    Add closing tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="theme-color" content="#ACCFF7" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" /> 
       </Head>
       <body>
         <main>

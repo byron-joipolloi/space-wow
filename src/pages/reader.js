73b8af740c79593b87c223c7b2deef6199e0f041
Byron <byron.walkermills@joipolloi.com>
2023-05-09 16:41:37 +0100

    Use rear facing camera

--- src/pages/reader.js
+++ src/pages/reader.js
@@ -13,6 +13,7 @@ export default function Reader({
     <div className="bg-[#fecee1] bg-[url('/bg-pink.png')] bg-center bg-no-repeat bg-cover">
       <div className="flex flex-col justify-between min-h-[100dvh] max-w-sm w-full mx-auto px-6 py-10 text-center space-y-12">
         <QrReader
+          constraints={{ facingMode: 'environment' }}
           onResult={(result, error) => {
             if (!!result) {
               const path = result?.text.replace('https://space-wow.vercel.app/?', '')

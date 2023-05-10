3ae7ac3ca452b1558ee87e02079c1c0a97d953da
Byron <byron.walkermills@joipolloi.com>
2023-05-10 10:59:58 +0100

    Slight update to qr reader style

--- src/pages/reader.js
+++ src/pages/reader.js
@@ -10,9 +10,10 @@ export default function Reader({
   const router = useRouter()
 
   return (
-    <div className="bg-[#fecee1] bg-[url('/bg-pink.png')] bg-center bg-no-repeat bg-cover">
-      <div className="flex flex-col justify-between min-h-[100dvh] max-w-sm w-full mx-auto px-6 py-10 text-center space-y-12">
+    <div className="bg-[#000] bg-center bg-no-repeat bg-cover">
+      <div className="flex flex-col justify-center min-h-[100dvh] max-w-sm w-full mx-auto px-6 py-10 text-center space-y-12">
         <QrReader
+          className="qr-reader"
           constraints={{ facingMode: 'environment' }}
           onResult={(result, error) => {
             if (!!result) {
@@ -34,9 +35,7 @@ export default function Reader({
               {/*console.info(error);*/}
             }
           }}
-          style={{ width: '100%' }}
         />
-        <p>{data}</p>
       </div>
     </div>
   )

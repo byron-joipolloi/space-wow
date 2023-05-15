15ed2fcc833f9754ebef2129543ef80eecc635ee
Byron <byron.walkermills@joipolloi.com>
2023-05-15 12:26:03 +0100

    Add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,13 @@ export default function Document() {
         <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f2b6da" />
+        <meta name="msapplication-TileColor" content="#f2b6da" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <main>
